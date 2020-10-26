78ccd3a192dace4acdba107bb0de599b97ac4af8
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-10-26 09:46:11 +0000

    feat: deleted old portfolio

--- README.md
+++
@@ -1,4 +0,0 @@
-# Github.io page for hosting web projects
-includes:
-home page,
-simple JavaScript web app showing progress through 2020 as a percentage in a progress bar,
--- pathfinder/Algorithms/aStar.js
+++
@@ -1,7 +0,0 @@
-function aStar () {
-    let unvisitedNodes = [];        //Array to hold all unvisited nodes
-    let visitedNodes = [];          //Array to dump visited nodes into
-    for(i = 0; i < grid[i].length; i++){
-        unvisitedNodes = unvisitedNodes.concat(grid[i]);        //populating unvisited nodes array to make life easier for sorting and retrieving closest unvisited node 
-    }
-}
